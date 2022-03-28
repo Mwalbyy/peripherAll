c0f50c250bfce7d0322e811f4e0e7c6fd8b0790d
Dylan Smith <dylanrs517@gmail.com>
2022-03-28 15:47:25 -0400

    added product routes

---
+++ controllers/api/productRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {Product} = require('../../models');
+
+router.get('/', async( req, res) =>{
+    try {
+        const productData = await Product.findAll()
+        const products = productData.map((product) => product.get({ plain:true}))
+        res.render('homepage', {
+            products
+        })
+    }catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+router.get('/product/:id', async (req, res) =>{
+    try {
+        const productData = await Product.findByPk(req.params.id, {});
+        res.render('product', {
+            productData
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
