64d67511fc08c6beb7cb894a9cb30e13c47b1398
Marcus Walby <marcuswalby24@gmail.com>
2022-03-28 15:43:03 -0400

    added homeroutes

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
-const apiRoutes = require('./api')
-// const  = require('express').Router();
-// const apiRoutes = require('./api')
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+
+module.exports = router;
