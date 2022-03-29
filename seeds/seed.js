0bc74242e40bbf386a1d3a35756bea67b5c756a7
Dylan Smith <dylanrs517@gmail.com>
2022-03-29 10:27:46 -0400

    added variables to seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,3 +1,18 @@
 const sequelize = require('../config/connection');
 const { User, Product, Review, Category } = require('../models');
 
+const categoryData = require('./categoryData.json');
+const productData = require('./productData.json');
+const reviewData = require('./reviewData.json');
+const userData = require('./userData.json');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+
+    
+
+
+    process.exit(0);
+};
+
+seedAll();
