719a17077d6626def61e3c800e105cfea569e1cb
Marcus Walby <marcuswalby24@gmail.com>
2022-03-30 14:05:23 -0400

    finised login feature and made sign up page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -25,5 +25,12 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get('/signup', async (req, res) => {
+  try {
+    res.render("signup")
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 const hbs = exphbs.create({});
 
 app.engine("handlebars", hbs.engine);
