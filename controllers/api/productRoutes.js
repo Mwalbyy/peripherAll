befa5647cb1dc90c38f0928ecbb53e14e6753e16
Dan Kim <danchanyoungkim@gmail.com>
2022-03-30 12:43:42 -0400

    Added model user adn attrivute user_name to product routes

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
-const {Product, Review} = require('../../models');
+const {Product, Review, User} = require('../../models');
 
 router.get('/', async (req, res) => {
     try {
         const productData = await Product.findAll({
           include: [
-            { model: Review, attributes: ['id', 'product_id', 'date_created', 'user_id', 'stars', 'text'] },
+            { model: Review, 
+            attributes: ['id', 'product_id', 'date_created', 'user_id', 'stars', 'text'],
+            include: {
+              model: User,
+              attributes: ['user_name']
+            }
+            },
           ],
         });
         const products = productData.map((product) => product.get({ plain:true }));
