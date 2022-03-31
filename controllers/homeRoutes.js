abe3c06284d0da27606a2f56fff91896ccaa6d03
Marcus Walby <marcuswalby24@gmail.com>
2022-03-30 20:50:33 -0400

    added testing for logged in  made logged in page w/code for login feature

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -31,4 +31,30 @@ router.get('/signup', async (req, res) => {
     res.status(500).json(err);
   }
 })
+
+// test route for login
+router.get("/logged", async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [
+        {
+          model: Product,
+          attributes: ['id', 'name', 'image'],
+        },
+      ],
+    });
+
+    const categories = categoryData.map((category) =>
+      category.get({ plain: true })
+      );
+      console.log(categories)
+    res.render("logged-in", {
+      categories
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// test route for login
+
 module.exports = router;
