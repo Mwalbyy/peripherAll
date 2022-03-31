9f9cc0a746760d0ae65d0f4bf5df6353190a4537
Marcus Walby <marcuswalby24@gmail.com>
2022-03-31 10:39:38 -0400

    finsihed signup page

--- models/user.js
+++ models/user.js
@@ -34,8 +34,8 @@ User.init(
     },
     {
         hooks: {
-          beforeCreate: async (newUserData) => {
-            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          beforeCreate: async (newUserData) => {newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            
             return newUserData;
           },
           beforeUpdate: async (updatedUserData) => {
