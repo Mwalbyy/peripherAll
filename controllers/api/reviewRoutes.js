57e3de6885441e49188c44715c9111685cf9a8a1
Dan Kim <danchanyoungkim@gmail.com>
2022-04-01 02:55:42 -0400

    Added dynamic to almost all properties in submitReview.js

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -14,10 +14,10 @@ router.get('/', async (req, res) => {
         
         const reviews = reviewData.map((review) => review.get({ plain: true }));
 
-        res.render('reviewpageall', {
-            reviews,
-        });
-        
+        // res.render('reviewpageall', {
+        //     reviews,
+        // });
+        res.status(200).json(reviews)
     } catch(err) {
         res.status(500).json(err);
     }
@@ -80,7 +80,7 @@ router.delete('/:id', async (req, res) => {
             res.status(404).json({ message: 'No review found with this id!' });
             return;
         }
-        res.render('review');
+        res.status(200).json(reviewData)
     } catch(err) {
         res.status(500).json(err);
     }
--- public/js/submitReview.js
+++ public/js/submitReview.js
@@ -1,24 +1,33 @@
+// Script for submitting reviews.
 async function submitFormHandler(event) {
     event.preventDefault();
 
-    const text = document.querySelector('#review-entry').value.trim();
+    // Calling the inputs from submit-review.handlebars.
+    const stars = document.querySelector('#rating-entry').value.trim(); // Variable for rating input.
+    const text = document.querySelector('#review-entry').value.trim(); // Variable for review input.
 
+    // Retrieves the search key in the url into a variable.
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
 
     const response = await fetch("/api/review", {
         method: "POST",
         body: JSON.stringify({
             text,
             user_id:1,
-            stars:4,
-            product_id:1,
+            stars,
+            product_id: id,
         }),
         headers: {
             "Content-Type": 'application/json',
         },
     });
 
+    // If response is good, then refresh page to same page once review is submitted.
     if (response.ok) {
-        document.location.replace('/api/product/1');
+        document.location.replace('/api/product/' + `${id}`);
+    // If not, give response error number.
     } else {
         alert(response.statusText);
     }
