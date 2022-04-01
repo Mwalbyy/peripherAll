bcc97e971bff516b6507d2fb52f307e1f37c9c18
Dan Kim <danchanyoungkim@gmail.com>
2022-04-01 11:30:33 -0400

    Add account-activity.handlebars

--- controllers/accountActivity.js
+++ controllers/accountActivity.js
@@ -1,4 +1,19 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Product, Review, Category } = require('../models');
-const withAuth = require('../utils/auth')
+const { Product, Review, Category, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req,res) => {
+    Review.findAll({
+        where: {
+            user_id: req.session.user_id,
+        },
+        attributes: ['id', 'product_id', 'date_created', 'stars', 'text', 'product'],
+        include: [
+            {
+                model: User,
+                attributes: ['user_name'],
+            },
+        ],
+    })
+})
