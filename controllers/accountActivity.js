2609f41dfd95cd9f73326d94a1af7a56c2abf257
Dan Kim <danchanyoungkim@gmail.com>
2022-04-01 12:09:34 -0400

    Add update=review.handlebars

--- controllers/accountActivity.js
+++ controllers/accountActivity.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Product, Review, Category, User } = require('../models');
 const withAuth = require('../utils/auth');
+const { post } = require('./api/reviewRoutes');
 
 router.get('/', withAuth, (req,res) => {
     Review.findAll({
@@ -16,4 +17,39 @@ router.get('/', withAuth, (req,res) => {
             },
         ],
     })
+        .then((reviewData) => {
+            const reviews = reviewData.map((review) => post.get({ plain: true }));
+            res.render('account-activity', {
+                reviews,
+                logged_in: true,
+                user_name: req.session.user_name,
+            });
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
+
+router.get('/update/:id', withAuth, (req,res) => {
+    Review.findOne({
+        where: {
+            id: req.params.id,
+        },
+        attributes: ['id', 'product_id', 'date_created', 'stars', 'text', 'product'],
+        include: [
+            {
+                model: User,
+                attributes: ['user_name'],
+            },
+        ],
+    })
+        .then((reviewData) => {
+            if (!reviewData) {
+                res.status(404).json({ message: 'No review found with this id' });
+                return;
+            }
+            
+            const review = reviewData.get({ plain: true });
+            console.log('sending' + req.session.user_name)
+        })
 })
