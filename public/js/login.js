b67faf71f2c6c52d722ea998288c4baee71ee7a6
Marcus Walby <marcuswalby24@gmail.com>
2022-04-04 00:13:53 -0400

    fixed signup

--- public/js/login.js
+++ public/js/login.js
@@ -19,30 +19,6 @@ if (email && password) {
     }
   }
 };
-
 document.querySelector('.signInForm').addEventListener('submit', loginFormHandler);
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-  console.log('hello')
-  
-  const name = document.querySelector("#name-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
-  
-  if (name && email && password) {
-    const response = await fetch("/api/users/signup", {
-      method: "POST",
-      body: JSON.stringify({ name, email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-    
-    if (response.ok) {
-      document.location.replace("/");
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
 
-document.querySelector('.signUpForm').addEventListener('submit', signupFormHandler);
